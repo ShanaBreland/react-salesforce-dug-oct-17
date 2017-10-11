5b25a9004a57d788da3063897c93f100ed7329e3
Roger Mitchell <rogeramitchell@gmail.com>
2017-10-11 18:42:04 -0400

    queued up starter files for the npm project

---
+++ start/webpack.config.js
@@ -0,0 +1,15 @@
+let path = require('path');
+
+module.exports = {
+	entry: './dev/js/App.js',
+	output: {
+		path: path.resolve(__dirname, './resource-bundles/OutlookUI.resource/js'),
+		filename: 'app.js'
+	},
+	module: {
+		loaders: [
+			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+			{ test: /\.css$/, loader: "style-loader!css-loader" }
+		]
+	}
+};
